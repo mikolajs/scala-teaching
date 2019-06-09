ad3a11ae483d68cb264e8f27b0216bee8d80fab7
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2019-06-09 19:56:03 +0200

    script for books sprinting

--- scripts/printpages.scala
+++ scripts/printpages.scala
@@ -9,10 +9,19 @@ if(args.length < 2) {
   System.exit(0);
 }
 
-val start = args(0).toInt
-val end = args(1).toInt
+val (start, endDeclared) = if(args(0).toInt < args(1).toInt) (args(0).toInt, args(1).toInt) else (args(1).toInt, args(0).toInt)
+val rest = if((endDeclared - start) % 4 == 0) {0} else {4 - ((endDeclared - start)) % 4 }
+val end = endDeclared + rest 
+var orderOdd:List[Int] = Nil
+var orderEven:List[Int] = Nil
+val quoter = (end - start) / 4
 
-val all = (start to end).toList
-
-println(all)
+for(i <- 0 until quoter){
+  orderOdd = (2*i+start)::orderOdd
+  orderOdd= (end - 2*i)::orderOdd
+  orderEven= (2*i+start+1)::orderEven
+  orderEven = (end -2*i -1)::orderEven
+}
+println(orderOdd.reverse.mkString(","))
+println(orderEven.reverse.mkString(","))
 
