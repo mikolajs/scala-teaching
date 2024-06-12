91e9764ef137e31ba621f67ad0fd2f558475f3e4
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2024-06-12 20:30:53 +0200

    gory

--- scripts/gory.scala
+++ scripts/gory.scala
@@ -1,5 +1,8 @@
 import scala.math
 import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
+
+val rand = Random()
 
 def isRectTriangle(a:Int, b:Int) =
   val c = math.floor(math.sqrt(a*a + b*b))
@@ -9,11 +12,35 @@ def findRectTriangles(N:Int) =
   val arrBuff = ArrayBuffer[(Int,Int)]() 
   for a <- 1 to N do
     for b <- 1 to N do
-      if isRectTriangle(a, b) then arrBuff += (a.toInt, b.toInt)
+      if isRectTriangle(a, b) then arrBuff += ((a.toInt, b.toInt))
   arrBuff
+
+def generateMountains(N:Int, ab:ArrayBuffer[(Int, Int)]) = 
+  val s = ab.size
+  var X = 0
+  var H = 100000
+  var points:List[(Int, Int)] = Nil
+  points = (X, H)::points
+  for i <- 1 to N do
+    val (x1, h1) = ab(rand.nextInt(s))
+    X += x1
+    H += h1
+    points = (X, H)::points
+    val (x, h) = ab(rand.nextInt(s))
+    X += x
+    H += h
+    points = (X, H)::points
+  points
+
+
+
+
 
 @main def main():Unit =
   val ab = findRectTriangles(10000)
   println(ab.size)
+  val mountains = generateMountains(20, ab)
+  for m <- mountains do
+    println(s"${m._1} ${m._2}")
 
 
--- scripts/kostki_i_tablice.scala
+++ scripts/kostki_i_tablice.scala
@@ -2,6 +2,16 @@ import scala.util.Random
 
 val rand = Random()
 
+def table(N:Int, M:Int):Unit =
+  for i <- 1 to N do
+    var list:List[Int] = Nil
+    for j <- 1 to M do
+       val s = rand.nextInt(190)-90
+       list = s::list
+    println(list.mkString(" "))
+
+def tableSq(N:Int) = table(N, N)
 
 @main def main():Unit = 
+  table(200, 400)
   
