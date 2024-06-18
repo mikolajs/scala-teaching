df61eb9bb026d05d2a264cc0c71974e6a7aee72a
Mikołaj Sochacki <mikolajsochacki@gmail.com>
2024-06-18 20:38:27 +0200

    kostki

--- scripts/kostki_i_tablice.scala
+++ scripts/kostki_i_tablice.scala
@@ -6,12 +6,17 @@ def table(N:Int, M:Int):Unit =
   for i <- 1 to N do
     var list:List[Int] = Nil
     for j <- 1 to M do
-       val s = rand.nextInt(190)-90
+       val s = rand.nextInt(201)
        list = s::list
     println(list.mkString(" "))
 
 def tableSq(N:Int) = table(N, N)
 
+def smallCube() =
+  for _ <- 1 to 20 do
+    println(rand.nextInt(90+5))
+
 @main def main():Unit = 
-  table(200, 400)
+  //table(10000, 100)
+  smallCube()
   
