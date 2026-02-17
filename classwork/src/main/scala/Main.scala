7823dd58b8b71b9527529f58d6025e97ca7f7588
Administrator <mikolajsochacki@gmail.com>
2026-02-17 14:32:45 +0100

    check classwork add paragraph

--- classwork/src/main/scala/Main.scala
+++ classwork/src/main/scala/Main.scala
@@ -1,5 +1,37 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+///https://www.jopendocument.org/docs/index.html
+import org.jopendocument.dom.text.TextDocument
+import org.jopendocument.dom.text.Paragraph
+import java.io.File
+import java.util.Scanner
 
-def msg = "I was compiled by Scala 3. :)"
+def openTxtFile(name:String):Array[String] =
+  var txt = ""
+  val txtFile = File(name)
+  if txtFile.exists() then
+    val scanner = Scanner(txtFile)
+    while scanner.hasNextLine() do
+      txt += scanner.nextLine() + "\n"
+  else 
+    println("ERROR FILE NOT EXISTS!")
+    txt = "Błąd brak pliku"
+  txt.split("\n")
+
+
+@main def hello(): Unit =
+  val outFile = File("test.odt")
+  if !outFile.exists() then
+    outFile.createNewFile()
+  val p1 = Paragraph("To jest pierwszy akapit z DOM Paragraph")
+  val doc = TextDocument.createEmpty("")
+  doc.add(p1)
+  val data = openTxtFile("pliktestu.txt")
+  for txt <- data do
+    doc.add(Paragraph(txt))
+  doc.saveAs(outFile)
+  org.jopendocument.dom.OOUtils.open(outFile)
+  //val engineTemp = RhinoTemplate(outFile)
+  //engineTemp.setField("Title", "To jest tytuł")
+  //engineTemp.saveAs(outFile)
+  
+
+
